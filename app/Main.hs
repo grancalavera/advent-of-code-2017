542d0321eefbb9ce038ae844ca4e0e38146f35a1
Leon Coto <leoncoto@gmail.com>
2018-04-17 08:36:07 +0100

    Day1: Base both examples on the same function

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,10 @@ main = putStrLn "Advent of Code"
 day1 :: IO ()
 day1 = do
   contents <- readFile "day1input.txt"
+  let contents' = concat $ lines contents
 
   putStrLn "Part 1"
-  print (captcha $ map digitToInt $ concat $ lines contents)
+  print (captcha 1 $ map digitToInt $ concat $ lines contents)
 
   putStrLn "Part 2"
-  print (kaptcha $ map digitToInt $ concat $ lines contents)
+  print (captcha (length contents' `div` 2) $ map digitToInt $ concat $ lines contents)
--- src/Advent/Day1.hs
+++ src/Advent/Day1.hs
@@ -1,21 +1,10 @@
 module Advent.Day1
 where
 
-captcha :: [Int] -> Int
-captcha []       = 0
-captcha (x : xs) = captcha' $ (x : xs) ++ [x]
+captcha :: Int -> [Int] -> Int
+captcha _      [] = 0
+captcha offset xs = captcha' (zip [0 ..] xs) (cycle xs)
  where
-  captcha' []       = 0
-  captcha' (x : xs) = captcha'' x xs
-  captcha'' last [] = 0
-  captcha'' last (next : rest) =
-    if last == next then last + captcha'' next rest else captcha' (next : rest)
-
-kaptcha :: [Int] -> Int
-kaptcha [] = 0
-kaptcha xs = kaptcha' (length xs `div` 2) (zip [0 ..] xs) (cycle xs)
- where
-  kaptcha' _ [] _ = 0
-  kaptcha' offset ((i, y) : iys) xs
-    | y == xs !! (i + offset) = y + kaptcha' offset iys xs
-    | otherwise               = kaptcha' offset iys xs
+  captcha' [] _ = 0
+  captcha' ((i, y) : iys) xs | y == xs !! (i + offset) = y + captcha' iys xs
+                             | otherwise               = captcha' iys xs
