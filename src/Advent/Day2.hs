c17da0203de90937ff9fad227aaa65e4d54a8306
Leon Coto <leoncoto@gmail.com>
2018-04-18 20:52:01 +0100

    Day2 -- Part 1🤘

--- src/Advent/Day2.hs
+++ src/Advent/Day2.hs
@@ -1,15 +1,20 @@
 module Advent.Day2
 where
 
-import           Data.Maybe                     ( isNothing )
+import           Data.Maybe                     ( isNothing
+                                                , fromJust
+                                                )
+
+checksum :: [[Int]] -> Int
+checksum xs = fromJust $ sum <$> mapM checksumRow xs
 
 checksumRow :: [Int] -> Maybe Int
-checksumRow _ = Just 0
+checksumRow xs = checksumRow' Nothing Nothing xs
 
 checksumRow' :: Maybe Int -> Maybe Int -> [Int] -> Maybe Int
 checksumRow' mn mx []       = (-) <$> mx <*> mn
 checksumRow' mn mx (x : xs) = checksumRow' mn' mx' xs
  where
-  mn' = choose min mn (pure x)
-  mx' = choose max mx (pure x)
-  choose f mx px = if isNothing mx then px else f <$> mn <*> px
+  px  = pure x
+  mn' = if isNothing mn then px else min <$> mn <*> px
+  mx' = if isNothing mx then px else max <$> mx <*> px
--- test/AdventSpec.hs
+++ test/AdventSpec.hs
@@ -21,7 +21,12 @@ spec = do
       captcha 3 [1, 2, 3, 1, 2, 3] `shouldBe` 12
       captcha 4 [1, 2, 1, 3, 1, 4, 1, 5] `shouldBe` 4
   describe "Day 2: Corruption Checksum" $ do
-    it "calculates the checksum of the spreadsheet" $ do
+    it "calculates the checksum of each row in the spreadsheet" $ do
       checksumRow [5, 1, 9, 5] `shouldBe` Just 8
       checksumRow [7, 5, 3] `shouldBe` Just 4
       checksumRow [2, 4, 6, 8] `shouldBe` Just 6
+    it "calculates the checksum of the spreadsheet" $ do
+      checksum [] `shouldBe` 0
+      checksum [ [5, 1, 9, 5]
+               , [7, 5, 3]
+               , [2, 4, 6, 8] ] `shouldBe` 18
