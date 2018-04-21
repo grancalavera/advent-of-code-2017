8208f8ad46f9c2ec5ef880989353d3e973959a92
Leon Coto <leoncoto@gmail.com>
2018-04-21 22:05:55 +0100

    Day 5 revisited: make a more efficient version using Data.Map.Strict

--- src/Advent/Day5.hs
+++ src/Advent/Day5.hs
@@ -1,14 +1,19 @@
 module Advent.Day5
 where
 
-escapeMaze :: [Int] -> Int
-escapeMaze maze = escapeMaze' 0 0 (zip ([0 ..] :: [Int]) maze)
+import qualified Data.Map.Strict               as Map
+import           Data.Map.Strict                ( Map
+                                                , (!?)
+                                                , (!)
+                                                )
+import           Data.Maybe                     ( isNothing )
 
-escapeMaze' :: Int -> Int -> [(Int, Int)] -> Int
-escapeMaze' _    _     []   = 0
-escapeMaze' jump jumps maze = if jump < 0 || jump >= length maze
+escapeMaze :: [Int] -> Int
+escapeMaze maze = escapeMaze' 0 0 (Map.fromList (zip ([0 ..] :: [Int]) maze))
+
+escapeMaze' :: Int -> Int -> Map Int Int -> Int
+escapeMaze' jump jumps maze = if isNothing (maze !? jump)
   then jumps
-  else
-    let jump' = jump + snd (maze !! jump)
-        maze' = map (\(i, x) -> if i == jump then (i, x + 1) else (i, x)) maze
-    in  escapeMaze' jump' (jumps + 1) maze'
+  else escapeMaze' (jump + maze ! jump)
+                   (jumps + 1)
+                   (Map.alter ((+ 1) <$>) jump maze)
